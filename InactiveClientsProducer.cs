f4d4918ec5c1deec5c1876d6f198cb4789818300
Gabriella Souza do Couto <gabriella@atlasgov.com>
2023-11-09 08:57:20 -0300

    Adding publisher classes

---
+++ InactiveClientsProducer.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRabbitMQ
+{
+    public class InactiveClientsProducer
+    {
+        public IModel? _channel { get; set; }
+        public string _message { get; set; }
+        public InactiveClientsProducer(IModel? channel, string message)
+        {
+            _channel = channel;
+            _message = message;
+        }
+
+        public void SendMessage()
+        {
+            var encodingMessage = Encoding.UTF8.GetBytes(_message);
+
+            _channel!.BasicPublish(exchange: "",
+                        routingKey: "inactive-clients",
+                        basicProperties: null,
+                        body: encodingMessage);
+
+            Console.WriteLine($"Enviou a mensagem {_message}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppRabbitMQ;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "active-clients",
+                    durable:false,
+                    exclusive:false,
+                    autoDelete: false,
+                    arguments: null);
+
+channel.QueueDeclare(queue: "inactive-clients",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+var activeClientsProducer = new ActiveClientsProducer(channel, "Leila Camargo");
+activeClientsProducer.SendMessage();
+
+var inactiveProducer = new InactiveClientsProducer(channel, "Denise Ferreira");
+inactiveProducer.SendMessage();
